312dbabaacfc69dcf45c79cba8f069c2e853177d
florian1107 <florian67@neuf.fr>
2020-01-27 13:15:22 +0100

    début controlleur admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -1,12 +1,29 @@
 <?php
 
 namespace App\Controller;
+use App\Entity\Article;
+
+use App\Repository\ArticleRepository;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+
 class AdminController extends AbstractController
 {
+    private $arcticleRepo;
+
+    public function __construct(ArticleRepository $articleRepository)
+    {
+        $this->articleRepo = $articleRepository;
+    }
+
+    public function admin_Home(Request $request){
+       
+        $article = $this->articleRepo->findAll();
+         return $this->render("admin/pages/admin_home.html.twig", [ "articles" => $articles,]);    
+    }
 
    
 }
