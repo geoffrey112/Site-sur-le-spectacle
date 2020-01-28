4cb3c5f38d1114de9e65c4bb87bf00eba03d8555
florian1107 <florian67@neuf.fr>
2020-01-28 10:45:31 +0100

    modif

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 use App\Entity\Article;
 use App\Entity\Category;
+use App\Entity\User;
 
 use App\Repository\ArticleRepository;
 use App\Repository\CategoryRepository;
+use App\Repository\UserRepository;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,18 +18,21 @@ class AdminController extends AbstractController
 {
     private $arcticleRepo;
     private $categoryRepo;
+    private $userRepo;
 
-    public function __construct(ArticleRepository $articleRepository, CategoryRepository $categoryRepository)
+    public function __construct(ArticleRepository $articleRepository, CategoryRepository $categoryRepository, UserRepository $userRepository)
     {
         $this->articleRepo = $articleRepository;
         $this->categoryRepo = $categoryRepository;
+        $this->userRepo = $userRepository;
     }
 
     public function admin_Home(){
        
         $article = $this->articleRepo->findAll();
         $category =$this->categoryRepo->findAll();
-         return $this->render("admin/pages/admin_home.html.twig", [ "articles" => $articles, "categories" => $categories]);    
+        $user =$this->userRepo->findAll();
+         return $this->render("admin/pages/admin_home.html.twig", [ "articles" => $articles, "categories" => $categories, "users" => $users]);    
     }
 
    
