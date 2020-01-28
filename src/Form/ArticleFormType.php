573c17b64ebbcd7034115dbc22bdd49299eaf661
florian1107 <florian67@neuf.fr>
2020-01-28 15:36:01 +0100

    formulaire création d'article

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -14,7 +14,7 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ArticleFormType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options) 
     {
         $builder
             ->add('title')
