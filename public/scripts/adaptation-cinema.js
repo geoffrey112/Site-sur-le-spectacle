3b90685b7cf56c7afe3bbefcec763453dba27523
florian1107 <florian67@neuf.fr>
2020-02-13 13:08:59 +0100

    modif js

--- public/scripts/adaptation-cinema.js
+++ public/scripts/adaptation-cinema.js
@@ -1,1 +1,27 @@
+$(document).ready(function() {
+    
+    $(window).scroll( function(){
+    
+        //Sélectionner l'élément que vous voulez animer//
+        $('.card').each( function(i){
+            
+            //Script permettant de detecter quand l'élément est visible dans votre fenètre//
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            if( bottom_of_window > bottom_of_object ){
+                
+                //Faites les animations que vous voulez//
+                $(this).animate({left: '60px'},3000);
+                    
+            }
+            
+        }); 
+    
+    });
+    
+});
 
+
+//Script à rajouter dans la base pour que ca marche//
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
