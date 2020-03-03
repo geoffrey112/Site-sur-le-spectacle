2bc03c14129e0cbf4e366bed6fae1e707a573741
florian1107 <florian67@neuf.fr>
2020-03-03 16:46:12 +0100

    anim page home

--- public/scripts/effet.js
+++ public/scripts/effet.js
@@ -9,5 +9,33 @@ $(document).ready(function() {
             $(this).animate({right:0, opacity:1 },2000);
           }
         });
+
+        $(".contentText").each(function(){
+            let distanceFromTop = $(this).offset().top;
+            if(scrolledFromTop >= distanceFromTop+100){
+              $(this).animate({left:0, opacity:1 },2000);
+            }
+        });
+
+        $(".contentRea").each(function(){
+            let distanceFromTop = $(this).offset().top;
+            if(scrolledFromTop >= distanceFromTop+100){
+              $(this).animate({right:0, opacity:1 },2000);
+            }
+        });
+
+        $(".contact").each(function(){
+            let distanceFromTop = $(this).offset().top;
+            if(scrolledFromTop >= distanceFromTop+0){
+              $(this).animate({right:0, opacity:1 },2000);
+            }
+        });
+
+        $(".form").each(function(){
+            let distanceFromTop = $(this).offset().top;
+            if(scrolledFromTop >= distanceFromTop+0){
+              $(this).animate({top:0, opacity:1 },2000);
+            }
+        });
     });
 });
